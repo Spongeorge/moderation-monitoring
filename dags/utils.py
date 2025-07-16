ff2b1d5d96babaa22cc1b1e02618c97859ef4388
George Baker <87348643+Spongeorge@users.noreply.github.com>
2025-07-16 14:39:41 -0600

    Add files via upload
    Add a utility to support multiple clients.

--- dags/utils.py
+++ dags/utils.py
@@ -1,7 +1,11 @@
 import praw
-from prawcore.exceptions import NotFound, Forbidden, TooManyRequests
+from prawcore.exceptions import NotFound, Forbidden, TooManyRequests, Redirect
 import time
 import random
+from sentence_transformers import SentenceTransformer
+from model2vec import StaticModel
+from typing import List, Dict
+import itertools
 
 
 def praw_retry(
@@ -22,8 +26,10 @@ def praw_retry(
                 return "NotFoundError"
             except Forbidden:
                 return "ForbiddenError"
+            except Redirect:
+                return "RedirectError"
             except AttributeError:
-                return "AttributeError" # if on Redditor, indicates banned account
+                return "AttributeError"  # if on Redditor, indicates banned account
             except Exception as e:
                 print(f"ERROR, RETRYING. {e}")
                 num_retries += 1
@@ -42,6 +48,45 @@ def praw_retry(
 def get_redditor(reddit: praw.Reddit, user_name: str) -> praw.models.Redditor:
     return reddit.redditor(user_name)
 
+
 @praw_retry
 def get_subreddit(reddit: praw.Reddit, subreddit_name: str) -> praw.models.Subreddit:
     return reddit.subreddit(subreddit_name)
+
+
+class SingletonSentenceTransformer:
+    """
+    Allows embedding model to be created once and kept in memory.
+    """
+    _instance = None
+
+    def __new__(cls, model_name='sentence-transformers/all-MiniLM-L6-v2'):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.model = SentenceTransformer(model_name)
+        return cls._instance
+
+
+class SingletonStaticModel:
+    """
+    Allows embedding model to be created once and kept in memory.
+    """
+    _instance = None
+
+    def __new__(cls, model_name='minishlab/potion-base-2M'):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.model = StaticModel.from_pretrained(model_name)
+        return cls._instance
+
+
+class Reddit(praw.Reddit):
+    def __init__(self, client_info: Dict | List[Dict]):
+        self.reddit_instances = [praw.Reddit(**kwargs) for kwargs in client_info] if isinstance(client_info,
+                                                                                                List) else [
+            praw.Reddit(**client_info)]
+        self._instance_cycle = itertools.cycle(self.reddit_instances)
+
+    def __getattr__(self, attr):
+        reddit = next(self._instance_cycle)
+        return getattr(reddit, attr)
